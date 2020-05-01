2499dcdffdaab4dcd9b1ba6a330b68cd429e6f43
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-05-01 19:09:48 +0200

    Prepare for 0.02 version

--- Optimization.pm
+++ Optimization.pm
@@ -6,7 +6,7 @@ use warnings;
 use Getopt::Std;
 use PYX::Optimization
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Constructor.
 sub new {
@@ -129,6 +129,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
